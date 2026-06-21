470bc30303652d72ddcd5df2bc7c1e1705a61a83
Parvathy <parvathytpav@gmail.com>
2026-06-21 21:33:50 +0530

    Add files via upload

---
+++ AES final.py
@@ -0,0 +1,15 @@
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+import base64
+key = get_random_bytes(16)
+
+message = "Cyber security internship project 1"
+cipher = AES.new(key, AES.MODE_EAX)
+encrypted, tag = cipher.encrypt_and_digest(message.encode())
+print("Encrypted Message:")
+print(base64.b64encode(encrypted).decode())
+
+cipher2 = AES.new(key, AES.MODE_EAX, nonce=cipher.nonce)
+decrypted = cipher2.decrypt(encrypted)
+print("\nDecrypted Message:")
+print(decrypted.decode())
